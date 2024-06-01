53f85a0629778655385d8653aa6cef377214d93a
rafsun <120895573+rafsun-sqa@users.noreply.github.com>
2024-06-01 16:09:37 +0600

    Update apiTask2.cy.js

--- apiTask2.cy.js
+++ apiTask2.cy.js
@@ -4,7 +4,7 @@ pm.test("Login successful", function () {
     pm.environment.set("authToken", jsonData.token);
 });
 pm.test("Items added to basket", function () {
-    pm.response.to.have.status(200); // Adjust as needed based on your API's response status
+    pm.response.to.have.status(200);
     var jsonData = pm.response.json();
     pm.expect(jsonData).to.be.an('array').that.has.lengthOf(2);
     pm.expect(jsonData[0]).to.have.property("item_id", "item123");
